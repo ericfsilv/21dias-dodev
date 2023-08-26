972af1afb5aac5c119ed50c98e04a25e6de83394
Eric Silva <ericfelipedev@gmail.com>
2023-08-26 15:53:31 -0300

    day 9'

--- day 9/index.js
+++ day 9/index.js
@@ -22,3 +22,32 @@ let dias = idade * 365
 console.log("Sua idade em meses é: " + meses)
 console.log("Sua idade em semanas é: " + semanas)
 console.log("Sua idade em dias é: " + dias)
+
+let imc = peso / (altura * altura)
+
+if(imc < 18.5) {
+    console.log("Magreza")
+} else if(imc >= 18.5 && imc <= 24.9) {
+    console.log("Normal")
+} else if(imc >= 25 && imc <=30) {
+    console.log("Sobrepeso")
+} else {
+    console.log("Obesidade")
+}
+
+let anoAtual = 2023
+let anoNasc = anoAtual - idade
+console.log("Voce nasceu no ano de " + anoNasc)
+
+let anoVivido = anoNasc
+let idadeAtual = 0
+
+for (let anoVivido = anoNasc; anoVivido <= anoAtual; anoVivido++) {
+    console.log(anoVivido + " - " + idadeAtual + " anos de idade") 
+    idadeAtual++
+}
+
+do {
+    let continuar = prompt("Deseja inserir os dados novamente? (s/n)")
+} while (continuar == "s")
+

733465d1360a148a3ebef1846bcee69c63f1d041
Eric Silva <ericfelipedev@gmail.com>
2023-11-17 16:23:22 -0300

    day 6

--- day 6/index.js
+++ day 6/index.js
@@ -19,10 +19,18 @@
 //}
 
 //TABUADA
-let numero = parseInt(prompt("Digite um numero inteiro e positivo"))
-for (let i = numero; i < numero + 2; i++) {
-    console.log("Tabuada do numero: " + i)
-    for (let j = 0; j <= 10; j++) {
-        console.log(i + "X" + j + " = " + (i * j))
-    } 
+//let numero = parseInt(prompt("Digite um numero inteiro e positivo"))
+//for (let i = numero; i < numero + 2; i++) {
+   // console.log("Tabuada do numero: " + i)
+   // for (let j = 0; j <= 10; j++) {
+   //     console.log(i + "X" + j + " = " + (i * j))
+    //} 
+//}
+
+let tabuada = prompt("Digite qual numero da tabuada deseja: ")
+
+for (let i = 0; i <= 10; i++) {
+    console.log(tabuada + " X " + i + " = " + (tabuada * i)) 
+    
 }
+
